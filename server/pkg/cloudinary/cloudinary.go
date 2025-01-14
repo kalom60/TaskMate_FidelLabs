c5f063a8e12c1b649ac65408ccfd3127d964b7cb
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-14 23:02:19 +0300

    cloudinary package with uplaod files method

---
+++ server/pkg/cloudinary/cloudinary.go
@@ -0,0 +1,90 @@
+package cloudinary
+
+import (
+	"context"
+	"fmt"
+	"mime/multipart"
+	"strings"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/admin"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+)
+
+type Cloudinary struct {
+	cld *cloudinary.Cloudinary
+}
+
+func NewCloudinaryService() (*Cloudinary, error) {
+	cld, err := cloudinary.New()
+	if err != nil {
+		return nil, fmt.Errorf("failed to intialize cloudinary: %w", err)
+	}
+
+	return &Cloudinary{
+		cld: cld,
+	}, nil
+}
+
+func (c *Cloudinary) UploadNewTaskFiles(ctx context.Context, files []*multipart.FileHeader, taskID string) ([]*uploader.UploadResult, error) {
+	folder, err := c.cld.Admin.CreateFolder(ctx, admin.CreateFolderParams{Folder: fmt.Sprintf("taskmate/%s", taskID)})
+	if err != nil {
+		return nil, fmt.Errorf("error while creating folder on cloudinary: %w", err)
+	}
+
+	type result struct {
+		uploaded *uploader.UploadResult
+		Err      error
+	}
+
+	resultChan := make(chan result, len(files))
+
+	for _, fileHeader := range files {
+		go func(fileHeader *multipart.FileHeader) {
+			file, err := fileHeader.Open()
+			if err != nil {
+				resultChan <- result{
+					uploaded: nil,
+					Err:      fmt.Errorf("failed to open file %s: %w", fileHeader.Filename, err),
+				}
+				return
+			}
+			defer file.Close()
+
+			uploadedFile, err := c.cld.Upload.Upload(ctx, file, uploader.UploadParams{
+				DisplayName: fileHeader.Filename,
+				AssetFolder: folder.Path,
+			})
+			if err != nil {
+				resultChan <- result{
+					uploaded: nil,
+					Err:      fmt.Errorf("failed to upload file %s: %w", fileHeader.Filename, err),
+				}
+				return
+			}
+
+			resultChan <- result{
+				uploaded: uploadedFile,
+				Err:      nil,
+			}
+		}(fileHeader)
+	}
+
+	var uploadedFiles []*uploader.UploadResult
+	var errs []string
+
+	for i := 0; i < len(files); i++ {
+		res := <-resultChan
+		if res.Err != nil {
+			errs = append(errs, res.Err.Error())
+		} else {
+			uploadedFiles = append(uploadedFiles, res.uploaded)
+		}
+	}
+
+	if len(errs) > 0 {
+		return uploadedFiles, fmt.Errorf("upload errors: %s", strings.Join(errs, ", "))
+	}
+
+	return uploadedFiles, nil
+}
