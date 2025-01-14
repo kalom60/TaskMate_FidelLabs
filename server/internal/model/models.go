91914d8b6cdb76c6a29a76fbbcb0e89369ca1b97
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-14 22:59:42 +0300

    model

---
+++ server/internal/model/models.go
@@ -0,0 +1,30 @@
+package models
+
+import "time"
+
+type Subtask struct {
+	ID        string    `json:"id" bson:"_id,omitempty"`
+	Title     string    `json:"title" bson:"title"`
+	Status    string    `json:"status" bson:"status"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
+}
+
+type File struct {
+	ID        string    `json:"id" bson:"_id,omitempty"`
+	FileName  string    `json:"fileName" bson:"fileName"`
+	FileURL   string    `json:"fileURL" bson:"fileURL"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
+}
+
+type Task struct {
+	ID          string    `json:"id" bson:"_id,omitempty"`
+	Title       string    `json:"title" bson:"title"`
+	Description string    `json:"description" bson:"description"`
+	DueDate     time.Time `json:"dueDate" bson:"dueDate"`
+	Status      string    `json:"status" bson:"status"`
+	Owner       string    `json:"owner" bson:"owner"`
+	Subtasks    []Subtask `json:"subtasks" bson:"subtasks"`
+	Files       []File    `json:"files" bson:"files"`
+}
