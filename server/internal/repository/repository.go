03fb13eeb52e4220e2e92fc19d60d52c027e6e21
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-14 22:59:58 +0300

    SaveNewTask method added

--- server/internal/repository/repository.go
+++ server/internal/repository/repository.go
@@ -1,10 +1,16 @@
 package repository
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/kalom60/TaskMate_FidelLabs/server/internal/database"
+	models "github.com/kalom60/TaskMate_FidelLabs/server/internal/model"
 )
 
-type Repository interface{}
+type Repository interface {
+	SaveNewTask(ctx context.Context, task models.Task) error
+}
 
 type repository struct {
 	client database.Service
@@ -14,4 +20,14 @@ func NewRepository(client database.Service) Repository {
 	return &repository{
 		client: client,
 	}
+}
+
+func (r *repository) SaveNewTask(ctx context.Context, task models.Task) error {
+	coll := r.client.GetCollection("taskmate", "task")
+
+	_, err := coll.InsertOne(ctx, task)
+	if err != nil {
+		return fmt.Errorf("error while saving new task: %w", err)
+	}
+	return nil
 }
