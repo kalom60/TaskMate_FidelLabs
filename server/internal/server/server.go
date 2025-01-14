d11fc9039d7664754964bef4bcb97f38bcc6c968
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-14 20:27:25 +0300

    add handler to Server struct

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -9,20 +9,28 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/kalom60/TaskMate_FidelLabs/server/internal/database"
+	"github.com/kalom60/TaskMate_FidelLabs/server/internal/handler"
+	"github.com/kalom60/TaskMate_FidelLabs/server/internal/repository"
 )
 
 type Server struct {
-	port int
-
-	db database.Service
+	port    int
+	db      database.Service
+	handler handler.Handler
 }
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	NewServer := &Server{
-		port: port,
 
-		db: database.New(),
+	db := database.New()
+
+	taskRepository := repository.NewRepository(db)
+	taskHandler := handler.NewHandler(taskRepository)
+
+	NewServer := &Server{
+		port:    port,
+		db:      db,
+		handler: taskHandler,
 	}
 
 	server := &http.Server{
