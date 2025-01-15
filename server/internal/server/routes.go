c3a723f53128ab5a06dd832c3f7b8aef841808a5
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-15 21:22:56 +0300

    sub task adding route

--- server/internal/server/routes.go
+++ server/internal/server/routes.go
@@ -20,6 +20,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	e.GET("/health", s.HealthHandler)
 
 	e.POST("/task", middlewares.ValidateTask(s.handler.NewTask))
+	e.POST("/task/:id", middlewares.ValidateSubTask(s.handler.AddSubTask))
 
 	return e
 }
