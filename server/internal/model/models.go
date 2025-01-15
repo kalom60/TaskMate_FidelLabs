67830da88b0cdb4f3fd0d7106e649f47669dd846
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-15 21:52:47 +0300

    update bson id on all models

--- server/internal/model/models.go
+++ server/internal/model/models.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type Subtask struct {
-	ID        string    `json:"id" bson:"_id,omitempty"`
+	ID        string    `json:"id" bson:"id"`
 	Title     string    `json:"title" bson:"title"`
 	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
 }
 
 type File struct {
-	ID        string    `json:"id" bson:"_id,omitempty"`
+	ID        string    `json:"id" bson:"id"`
 	FileName  string    `json:"fileName" bson:"fileName"`
 	FileURL   string    `json:"fileURL" bson:"fileURL"`
 	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
@@ -18,7 +18,7 @@ type File struct {
 }
 
 type Task struct {
-	ID          string    `json:"id" bson:"_id,omitempty"`
+	ID          string    `json:"id" bson:"id"`
 	Title       string    `json:"title" bson:"title"`
 	Description string    `json:"description" bson:"description"`
 	DueDate     time.Time `json:"dueDate" bson:"dueDate"`
