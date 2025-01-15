fc75249f1df011de78aaf032339353bf071862c8
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-15 23:08:37 +0300

    use models.SubTask for binding type

--- server/pkg/middlewares/validateSubTask.go
+++ server/pkg/middlewares/validateSubTask.go
@@ -4,24 +4,26 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	models "github.com/kalom60/TaskMate_FidelLabs/server/internal/model"
 	"github.com/labstack/echo/v4"
 )
 
-type SubTaskForm struct {
-	Title string `validate:"required"`
-}
-
 func ValidateSubTask(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		subTaskform := SubTaskForm{
-			Title: c.FormValue("title"),
+		var subTask models.Subtask
+
+		if err := c.Bind(&subTask); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid input")
 		}
 
 		validate := validator.New()
-		if err := validate.Struct(subTaskform); err != nil {
+		if err := validate.Struct(subTask); err != nil {
 			errors := err.(validator.ValidationErrors)
 			return echo.NewHTTPError(http.StatusBadRequest, errors.Error())
 		}
+
+		c.Set("subTask", subTask)
+
 		return next(c)
 	}
 }
