cc73842401aa955e911c943d4adfcc2821acc693
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-28 22:37:00 +0300

    delete confirmation

---
+++ client/src/components/kanban/DeleteConfirmation.tsx
@@ -0,0 +1,43 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "../ui/alert-dialog";
+
+interface DeleteConfirmationProps {
+  open: boolean;
+  title: string;
+  message: string;
+  onOpenChange: (open: boolean) => void;
+  onConfirm: () => void;
+}
+
+const DeleteConfirmation = ({
+  open,
+  title,
+  message,
+  onOpenChange,
+  onConfirm,
+}: DeleteConfirmationProps) => {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{message}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteConfirmation;
