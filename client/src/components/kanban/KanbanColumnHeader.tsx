53e84efc67189e5835c70a096e9f61e2ed199aa4
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-28 22:36:43 +0300

    kanban column header

---
+++ client/src/components/kanban/KanbanColumnHeader.tsx
@@ -0,0 +1,60 @@
+import { TaskStatus } from "@/utils/types";
+import {
+  CircleCheckIcon,
+  CircleDotDashedIcon,
+  CircleIcon,
+  PlusIcon,
+} from "lucide-react";
+import { Button } from "../ui/button";
+import CreateTaskModal from "../task/CreateTaskModal";
+import { useState } from "react";
+
+interface KanbanColumnHeaderProps {
+  board: TaskStatus;
+  taskCount: number;
+}
+
+const statusIconMap: Record<TaskStatus, React.ReactNode> = {
+  [TaskStatus.NOT_STARTED]: <CircleIcon className="size-[18px] text-red-400" />,
+  [TaskStatus.IN_PROGRESS]: (
+    <CircleDotDashedIcon className="size-[18px] text-yellow-400" />
+  ),
+  [TaskStatus.COMPLETED]: (
+    <CircleCheckIcon className="size-[18px] text-emerald-400" />
+  ),
+};
+
+const KanbanColumnHeader = ({ board, taskCount }: KanbanColumnHeaderProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const icon = statusIconMap[board];
+
+  return (
+    <>
+      <div className="px-2 py-1.5 flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          {icon}
+          <h2 className="text-sm font-medium">{board}</h2>
+          <div className="size-5 flex items-center justify-center rounded-md bg-neutral-200 text-xs text-neutral-700 font-medium">
+            {taskCount}
+          </div>
+        </div>
+        <Button
+          onClick={() => setOpen(true)}
+          variant={"ghost"}
+          size={"icon"}
+          className="size-5"
+        >
+          <PlusIcon className="size-4 text-neutral-500" />
+        </Button>
+      </div>
+
+      <CreateTaskModal
+        open={open}
+        onOpenChange={() => setOpen(false)}
+        status={board}
+      />
+    </>
+  );
+};
+
+export default KanbanColumnHeader;
