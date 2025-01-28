adc15c0c5675bc736910c410cab65bae6de44d57
kalom60 <39136564+kalom60@users.noreply.github.com>
2025-01-28 22:42:27 +0300

    create subtask modal hook

---
+++ client/src/hooks/useCreateSubtaskModal.ts
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useCreateSubtask } from "@/hooks/useCreateSubtask";
+
+interface UseCreateSubtaskModalProps {
+  id: string;
+  onSuccess?: () => void;
+}
+
+export const useCreateSubtaskModal = ({
+  id,
+  onSuccess,
+}: UseCreateSubtaskModalProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { mutate: createSubtask, isPending } = useCreateSubtask({
+    onSuccess: () => {
+      setIsOpen(false);
+      onSuccess?.();
+    },
+    onCancel: () => setIsOpen(false),
+  });
+
+  const openModal = () => setIsOpen(true);
+
+  const closeModal = () => setIsOpen(false);
+
+  const handleSubmit = (values: { title: string }) => {
+    createSubtask({ subtask: values, id: id });
+  };
+
+  return {
+    isOpen,
+    openModal,
+    closeModal,
+    handleSubmit,
+    isPending,
+  };
+};
